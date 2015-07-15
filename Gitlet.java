a69d94ba5ce9a4f8d38ba93b20cf72e50fc43aaf
David Yan <david.yan@berkeley.edu>
2015-07-15 14:42:00 -0700

    merge test

--- Gitlet.java
+++ Gitlet.java
@@ -6,10 +6,11 @@ public class Gitlet
 		System.out.println("Hi");
 		System.out.println("TEST");
 		System.out.println("Github is working correctly.");
-		
+
 	}
-	
-	public void Test(){
+
+	public void Test()
+	{
 		System.out.println("Test");
 	}
 	public static void David()
