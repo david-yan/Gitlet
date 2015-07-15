f99fae3da38917910c4ad9d11b913f7bd9fb05ba
David Yan <david.yan@berkeley.edu>
2015-07-15 14:30:12 -0700

    test 4

--- Gitlet.java
+++ Gitlet.java
@@ -1,9 +1,10 @@
 public class Gitlet
 {
-  public static void main(String[] args)
-  {
-	  System.out.println("Hello");
-	  System.out.println("Hi");
-	  System.out.println("TEST");
-  }
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		System.out.println("Hi");
+		System.out.println("TEST");
+		System.out.println("Github is working correctly.");
+	}
 }
