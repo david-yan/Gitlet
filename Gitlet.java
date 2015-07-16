27c8e8927a894c0c763ebafc5635f86c833991ca
David Yan <david.yan@berkeley.edu>
2015-07-15 21:53:11 -0700

    Initializing .gitlet folder working.

--- Gitlet.java
+++ Gitlet.java
@@ -1,7 +1,21 @@
+import java.io.File;
+
 public class Gitlet
 {
-	public static void main(String[] args)
+	public Gitlet()
 	{
 		
+	}
+	public static void main(String[] args)
+	{
+		if(args[0].equals("init"))
+		{
+			File gitletDir = new File(".gitlet");
+			if (!gitletDir.exists())
+			{
+				gitletDir.mkdir();
+				System.out.println("Gitlet directory created.");
+			}
+		}
 	}
 }
