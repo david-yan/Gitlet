e0bf6e53c72f0a92e9997594b71539351288d979
David Yan <david.yan@berkeley.edu>
2015-07-15 19:16:20 -0700

    Necessary classes added, and testing code removed.

--- Gitlet.java
+++ Gitlet.java
@@ -2,23 +2,6 @@ public class Gitlet
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello");
-		System.out.println("Hi");
-		System.out.println("TEST");
-		System.out.println("Github is working correctly.");
-
-	}
-
-	public void Test()
-	{
-		System.out.println("Test");
-	}
-	public static void David()
-	{
-		System.out.println("David");
-	}
-	
-	public static void Quang() {
-		System.out.println("test");
+		
 	}
 }
