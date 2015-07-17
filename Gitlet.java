714ec705124240c429d30a15ec454499e2dd3bf7
quangnguyen <quangnguyen@quangs-air.t-mobile.com>
2015-07-17 09:39:11 -0700

    test to push onto github

--- Gitlet.java
+++ Gitlet.java
@@ -1,58 +1,52 @@
-
 import java.io.*;
 
-public class Gitlet implements Serializable
-{
-	public Gitlet()
-	{
+public class Gitlet implements Serializable {
+	public Gitlet() {
 		File gitletDir = new File(".gitlet");
-		if (!gitletDir.exists())
-		{
+		if (!gitletDir.exists()) {
 			gitletDir.mkdir();
 			this.commit("initial commit");
 		}
 	}
-	public void commit(String message)
-	{
+
+	public void commit(String message) {
 
 	}
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		Gitlet gitlet = null;
 		boolean gitletExists = false;
-		try
-		{
-			FileInputStream fileIn = new FileInputStream(new File(".gitlet", "Gitlet.ser"));
+		try {
+			FileInputStream fileIn = new FileInputStream(new File(".gitlet",
+					"Gitlet.ser"));
 			ObjectInputStream in = new ObjectInputStream(fileIn);
 			gitlet = (Gitlet) in.readObject();
 			gitletExists = true;
 			in.close();
 			fileIn.close();
-		} catch (FileNotFoundException e)
-		{
-		} catch (IOException e)
-		{
-		} catch (ClassNotFoundException e)
-		{
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		} catch (ClassNotFoundException e) {
 		}
-		if (args[0].equals("init"))
-		{
-			if (!gitletExists)
-			{
+		if (args[0].equals("init")) {
+			if (!gitletExists) {
 				gitlet = new Gitlet();
-			} else
-				System.err.println("A gitlet version control system already exists in the current directory.");
+			} else {
+				System.err
+						.println("A gitlet version control system already exists in the current directory.");
+				return;
+			}
 		}
-		try
-		{
-			FileOutputStream fileOut = new FileOutputStream(new File(".gitlet", "Gitlet.ser"));
+
+		try {
+			FileOutputStream fileOut = new FileOutputStream(new File(".gitlet",
+					"Gitlet.ser"));
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
 			out.writeObject(gitlet);
 			out.close();
 			fileOut.close();
 			System.out.println("Gitlet written in file.");
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
