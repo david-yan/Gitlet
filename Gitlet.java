854d4bc6533b896996531b9b731883fe99621c28
quangnguyen <quangnguyen@quangs-air.t-mobile.com>
2015-07-17 09:18:30 -0700

    1st commit

--- Gitlet.java
+++ Gitlet.java
@@ -1,14 +1,59 @@
-public class Gitlet
+
+import java.io.*;
+
+public class Gitlet implements Serializable
 {
+	public Gitlet()
+	{
+		File gitletDir = new File(".gitlet");
+		if (!gitletDir.exists())
+		{
+			gitletDir.mkdir();
+			this.commit("initial commit");
+		}
+	}
+	public void commit(String message)
+	{
+
+	}
 	public static void main(String[] args)
 	{
-		System.out.println("Hello");
-		System.out.println("Hi");
-		System.out.println("TEST");
-		System.out.println("Github is working correctly.");
-	}
-	
-	public static void Quang() {
-		System.out.println("test");
+		Gitlet gitlet = null;
+		boolean gitletExists = false;
+		try
+		{
+			FileInputStream fileIn = new FileInputStream(new File(".gitlet", "Gitlet.ser"));
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			gitlet = (Gitlet) in.readObject();
+			gitletExists = true;
+			in.close();
+			fileIn.close();
+		} catch (FileNotFoundException e)
+		{
+		} catch (IOException e)
+		{
+		} catch (ClassNotFoundException e)
+		{
+		}
+		if (args[0].equals("init"))
+		{
+			if (!gitletExists)
+			{
+				gitlet = new Gitlet();
+			} else
+				System.err.println("A gitlet version control system already exists in the current directory.");
+		}
+		try
+		{
+			FileOutputStream fileOut = new FileOutputStream(new File(".gitlet", "Gitlet.ser"));
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(gitlet);
+			out.close();
+			fileOut.close();
+			System.out.println("Gitlet written in file.");
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
