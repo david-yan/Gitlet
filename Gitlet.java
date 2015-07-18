26b166c27b4a0dc071350125ce9a4563cbea946e
quangnguyen <quangnguyen@quangs-air.t-mobile.com>
2015-07-17 23:13:04 -0700

    first init and log showing commit 1 works

--- Gitlet.java
+++ Gitlet.java
@@ -1,16 +1,43 @@
 import java.io.*;
 
 public class Gitlet implements Serializable {
+
+	private int numberOfCommit;
+	private GitletNode currentBranchHead;
+
 	public Gitlet() {
 		File gitletDir = new File(".gitlet");
 		if (!gitletDir.exists()) {
 			gitletDir.mkdir();
-			this.commit("initial commit");
+			this.commit();
 		}
+		numberOfCommit = 1;
+		numberOfCommit++;
 	}
 
 	public void commit(String message) {
-		System.out.println("test");
+		GitletNode commitNode = new GitletNode(message, numberOfCommit, currentBranchHead);
+	}
+
+	public void commit() {
+		GitletNode initialCommit = new GitletNode();
+		// try {
+		// FileOutputStream commitToFile = new FileOutputStream(new File(
+		// "commit#1.ser"));
+		// ObjectOutputStream out = new ObjectOutputStream(commitToFile);
+		// out.writeObject(initialCommit);
+		// out.close();
+		// commitToFile.close();
+		// System.out.println("GitletNode written in file.");
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		currentBranchHead = initialCommit;
+	}
+
+	public void log() {
+		currentBranchHead.printLog();
 	}
 
 	public static void main(String[] args) {
@@ -22,20 +49,35 @@ public class Gitlet implements Serializable {
 			ObjectInputStream in = new ObjectInputStream(fileIn);
 			gitlet = (Gitlet) in.readObject();
 			gitletExists = true;
+//			System.out.println("in here");
 			in.close();
 			fileIn.close();
 		} catch (FileNotFoundException e) {
 		} catch (IOException e) {
 		} catch (ClassNotFoundException e) {
 		}
-		if (args[0].equals("init")) {
+
+		if (args.length == 0) {
+			System.err.println("Please enter a command.");
+		}
+
+		else if (args[0].equals("log")) {
+//			System.out.println(gitlet.currentBranchHead);
+			gitlet.log();
+		}
+
+		else if (args[0].equals("init")) {
 			if (!gitletExists) {
 				gitlet = new Gitlet();
+//				System.out.println(gitlet.currentBranchHead.getTimeStamp());
 			} else {
 				System.err
 						.println("A gitlet version control system already exists in the current directory.");
-				return;
 			}
+		}
+
+		else {
+			System.err.println("No command with that name exists.");
 		}
 
 		try {
@@ -49,5 +91,7 @@ public class Gitlet implements Serializable {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
+
 }
--- GitletNode.java
+++ GitletNode.java
@@ -1,4 +1,40 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.io.*;
 
-public class GitletNode {
-	private int test;
+public class GitletNode implements Serializable {
+	private GitletNode prevCommit;
+	private String commitMessage;
+	private String timeStamp;
+	private int commitID;
+	
+	public GitletNode() {
+		prevCommit = null;
+		commitMessage = "initial commit";
+		timeStamp = getTimeStamp();
+		commitID = 1;
+	}
+	
+	public String getTimeStamp() {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+	}
+	
+	public void printLog() {
+		System.out.println("===");
+		System.out.println("Commit " + commitID);
+		System.out.println(timeStamp);
+		System.out.println(commitMessage);
+		if (prevCommit != null) {
+			System.out.println();
+			prevCommit.printLog();
+		}
+	}
+	
+	public GitletNode(String message, int ID, GitletNode prev) {
+		prevCommit = prev;
+		commitMessage = message;
+		timeStamp = getTimeStamp();
+		commitID = ID;
+	}
+	
 }
