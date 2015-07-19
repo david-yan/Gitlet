ec09e8ac41bdc0149550c4c062ca56f079f75bef
quangnguyen <quangnguyen@quangs-air.t-mobile.com>
2015-07-19 12:02:24 -0700

    init, add, rm, commit works for 1 commit branch, need to make tests

--- Gitlet.java
+++ Gitlet.java
@@ -1,43 +1,225 @@
 import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.file.*;
+import java.util.*;
+
+import static java.nio.file.StandardCopyOption.*;
 
 public class Gitlet implements Serializable {
 
 	private int numberOfCommit;
 	private GitletNode currentBranchHead;
+	// keep track of branches
+	// key is the name of the branch
+	// value is possibly the LinkedList of GitletNodes
+	private HashMap<String, GitletNode> branches;
+	private File stagingDir = new File(".gitlet/staging");
+	private HashSet<String> untrack;
 
 	public Gitlet() {
 		File gitletDir = new File(".gitlet");
+		numberOfCommit = 0;
+		untrack = new HashSet<String>();
 		if (!gitletDir.exists()) {
 			gitletDir.mkdir();
-			this.commit();
+			stagingDir.mkdir();
+			commit("initial commit");
 		}
-		numberOfCommit = 1;
-		numberOfCommit++;
 	}
 
 	public void commit(String message) {
-		GitletNode commitNode = new GitletNode(message, numberOfCommit, currentBranchHead);
+
+		if (numberOfCommit != 0 && stagingDir.list().length == 0
+				&& untrack.isEmpty()) {
+			System.err.println("No changes added to the commit.");
+			return;
+		}
+
+		GitletNode commitNode = new GitletNode(message, numberOfCommit,
+				currentBranchHead);
+
+		if (numberOfCommit > 0) {
+
+			File newCommit = commitNode.getContents();
+
+			try {
+
+				/*
+				 * put all the marked files from parent's and also any files in
+				 * staging folder into contents if files from parent's have the
+				 * same name as files from staging folder, select the files from
+				 * staging folder since those are the updated ones check files
+				 * that are marked "untrack" and do not include those in the
+				 * commit
+				 */
+				copyToNewCommit(newCommit);
+				/*
+				 * at the end of commit staging folder should be empty so we
+				 * move files from staging to the new commit
+				 */
+				moveFromStagingToNewCommit(newCommit);
+				untrack.clear();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		currentBranchHead = commitNode;
+		numberOfCommit++;
+
+		System.out.println("Commit successful");
 	}
 
-	public void commit() {
-		GitletNode initialCommit = new GitletNode();
-		// try {
-		// FileOutputStream commitToFile = new FileOutputStream(new File(
-		// "commit#1.ser"));
-		// ObjectOutputStream out = new ObjectOutputStream(commitToFile);
-		// out.writeObject(initialCommit);
-		// out.close();
-		// commitToFile.close();
-		// System.out.println("GitletNode written in file.");
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		currentBranchHead = initialCommit;
+	/*
+	 * @author
+	 * "http://stackoverflow.com/questions/22356585/moving-files-from-one-directory-to-another-with-java-nio"
+	 * Files.move won't detect that the destination is a directory; therefore,
+	 * we have to construct full destination path, file by file
+	 */
+	private void moveFromStagingToNewCommit(File newCommit) throws IOException {
+		for (File file : stagingDir.listFiles()) {
+			File newCommitPath = new File(newCommit, file.getName());
+			Files.move(file.toPath(), newCommitPath.toPath(), REPLACE_EXISTING);
+		}
+	}
+
+	/*
+	 * copy files from current commit that were unchanged or NOT marked for
+	 * untracking
+	 */
+	private void copyToNewCommit(File newCommit) throws IOException {
+		File pathOfCurrentCommit = currentBranchHead.getContents();
+		for (File file : pathOfCurrentCommit.listFiles()) {
+			if (!file.isDirectory()
+					&& !inStagingDir(stagingDir, file.getName())
+					&& !untrack.contains(file.getName())) {
+				File newCommitPath = new File(newCommit, file.getName());
+				copyFileUsingFileChannels(file, newCommitPath);
+			}
+		}
 	}
 
 	public void log() {
 		currentBranchHead.printLog();
+	}
+
+	// should only add files, not folders / directory
+	public void add(String fileName) {
+
+		// fileName could be a path to the file
+		File fileToAdd = new File(fileName);
+
+		/*
+		 * check if file exists in any directories or its subdirectries of the
+		 * working directory or its in the working directory ( which is the same
+		 * directory that contains the .gitlet folder )
+		 */
+		if (fileToAdd.isDirectory() || !fileToAdd.exists()) {
+			System.err.println("File does not exist.");
+			return;
+		}
+
+		// if it was marked for "untracking", just unmark it
+		if (untrack.contains(fileName)) {
+			untrack.remove(fileName);
+			System.out.println("file is now unmark for \"untracking\"");
+			return;
+		}
+
+		// put it in the staging folder
+		try {
+
+			// what if file was in a directory?
+			// need to get the file name and not the path
+			// use File.getname()
+
+			File toStagingDir = new File(".gitlet/staging/"
+					+ fileToAdd.getName());
+			copyFileUsingFileChannels(fileToAdd, toStagingDir);
+
+			System.out.println("moved file to add into staging folder");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void remove(String fileName) {
+		// if file is not in staging folder
+		// or it's not tracked by head commit
+
+		// System.out.println(!inStagingDir(stagingDir, fileName));
+		// System.out.println(!inHeadCommit(currentBranchHead.getContents(),
+		// fileName));
+		if (!inStagingDir(stagingDir, fileName)
+				&& !inHeadCommit(currentBranchHead.getContents(), fileName)) {
+			System.err.println("No reason to remove the file.");
+			return;
+		}
+
+		// if fileName is in staging folder
+		// remove it from staging folder
+		if (inStagingDir(stagingDir, fileName)) {
+			unstageFile(stagingDir, fileName);
+
+			System.out.println("file unstaged");
+		}
+
+		// put it in untrack HashSet
+		else if (!untrack.contains(fileName)) {
+			untrack.add(fileName);
+			System.out.println("file untracked");
+		} else {
+			System.out.println("called rm but nothing happened");
+		}
+
+	}
+
+	private static void unstageFile(File currentDir, String fileName) {
+		for (File file : currentDir.listFiles()) {
+			if (!file.isDirectory() && file.getName().equals(fileName)) {
+				file.delete();
+				return;
+			}
+		}
+	}
+
+	private static boolean inHeadCommit(File currentDir, String fileName) {
+		for (File file : currentDir.listFiles()) {
+			if (!file.isDirectory() && file.getName().equals(fileName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean inStagingDir(File currentDir, String fileName) {
+		for (File file : currentDir.listFiles()) {
+			if (!file.isDirectory() && file.getName().equals(fileName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * @author
+	 * "http://examples.javacodegeeks.com/core-java/io/file/4-ways-to-copy-file-in-java/"
+	 * make a copy of source and move it to dest
+	 */
+	private static void copyFileUsingFileChannels(File source, File dest)
+			throws IOException {
+		FileChannel inputChannel = null;
+		FileChannel outputChannel = null;
+		try {
+			inputChannel = new FileInputStream(source).getChannel();
+			outputChannel = new FileOutputStream(dest).getChannel();
+			outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
+		} finally {
+			inputChannel.close();
+			outputChannel.close();
+		}
 	}
 
 	public static void main(String[] args) {
@@ -49,7 +231,6 @@ public class Gitlet implements Serializable {
 			ObjectInputStream in = new ObjectInputStream(fileIn);
 			gitlet = (Gitlet) in.readObject();
 			gitletExists = true;
-//			System.out.println("in here");
 			in.close();
 			fileIn.close();
 		} catch (FileNotFoundException e) {
@@ -61,15 +242,32 @@ public class Gitlet implements Serializable {
 			System.err.println("Please enter a command.");
 		}
 
+		else if (args[0].equals("commit")) {
+
+			if (args.length == 2 && args[1].trim().length() != 0) {
+				gitlet.commit(args[1]);
+			} else {
+				System.err.println("Please enter a commit messsage.");
+			}
+
+		}
+
+		else if (args[0].equals("add")) {
+			gitlet.add(args[1]);
+
+		}
+
+		else if (args[0].equals("rm")) {
+			gitlet.remove(args[1]);
+		}
+
 		else if (args[0].equals("log")) {
-//			System.out.println(gitlet.currentBranchHead);
 			gitlet.log();
 		}
 
 		else if (args[0].equals("init")) {
 			if (!gitletExists) {
 				gitlet = new Gitlet();
-//				System.out.println(gitlet.currentBranchHead.getTimeStamp());
 			} else {
 				System.err
 						.println("A gitlet version control system already exists in the current directory.");
@@ -93,5 +291,4 @@ public class Gitlet implements Serializable {
 		}
 
 	}
-
 }
--- GitletNode.java
+++ GitletNode.java
@@ -7,18 +7,15 @@ public class GitletNode implements Serializable {
 	private String commitMessage;
 	private String timeStamp;
 	private int commitID;
-	
+	private File contents;
+
 	public GitletNode() {
-		prevCommit = null;
-		commitMessage = "initial commit";
-		timeStamp = getTimeStamp();
-		commitID = 1;
 	}
-	
+
 	public String getTimeStamp() {
 		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 	}
-	
+
 	public void printLog() {
 		System.out.println("===");
 		System.out.println("Commit " + commitID);
@@ -29,12 +26,22 @@ public class GitletNode implements Serializable {
 			prevCommit.printLog();
 		}
 	}
-	
+
 	public GitletNode(String message, int ID, GitletNode prev) {
 		prevCommit = prev;
 		commitMessage = message;
 		timeStamp = getTimeStamp();
 		commitID = ID;
+		contents = new File(".gitlet/" + commitID);
+		contents.mkdir();
 	}
 	
+	public GitletNode getPrevCommit() {
+		return prevCommit;
+	}
+	
+	public File getContents() {
+		return contents;
+	}
+
 }
