d15ec3b312b1b00d0572e90b6c65f4ca81faa6d2
david-yan <david.yan@berkeley.edu>
2015-07-19 14:10:56 -0700

    Update Gitlet.java

--- Gitlet.java
+++ Gitlet.java
@@ -1,61 +1,4 @@
-import java.io.*;
-<<<<<<< HEAD
 
-public class Gitlet implements Serializable
-{
-	public Gitlet()
-	{
-		File gitletDir = new File(".gitlet");
-		if (!gitletDir.exists())
-		{
-			gitletDir.mkdir();
-			this.commit("initial commit");
-		}
-	}
-	public void commit(String message)
-	{
-
-	}
-	public static void main(String[] args)
-	{
-		Gitlet gitlet = null;
-		boolean gitletExists = false;
-		try
-		{
-			FileInputStream fileIn = new FileInputStream(new File(".gitlet", "Gitlet.ser"));
-			ObjectInputStream in = new ObjectInputStream(fileIn);
-			gitlet = (Gitlet) in.readObject();
-			gitletExists = true;
-			in.close();
-			fileIn.close();
-		} catch (FileNotFoundException e)
-		{
-		} catch (IOException e)
-		{
-		} catch (ClassNotFoundException e)
-		{
-		}
-		if (args[0].equals("init"))
-		{
-			if (!gitletExists)
-			{
-				gitlet = new Gitlet();
-			} else
-				System.err.println("A gitlet version control system already exists in the current directory.");
-		}
-		try
-		{
-			FileOutputStream fileOut = new FileOutputStream(new File(".gitlet", "Gitlet.ser"));
-			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(gitlet);
-			out.close();
-			fileOut.close();
-			System.out.println("Gitlet written in file.");
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-=======
 import java.nio.channels.FileChannel;
 import java.nio.file.*;
 import java.util.*;
@@ -125,15 +68,8 @@ public class Gitlet implements Serializable {
 		numberOfCommit++;
 
 //		System.out.println("Commit successful");
->>>>>>> branch 'quang' of https://github.com/siddharthseth/gitlet-cs61bl.git
 	}
 
-	/*
-	 * @author
-	 * "http://stackoverflow.com/questions/22356585/moving-files-from-one-directory-to-another-with-java-nio"
-	 * Files.move won't detect that the destination is a directory; therefore,
-	 * we have to construct full destination path, file by file
-	 */
 	private void moveFromStagingToNewCommit(File newCommit) throws IOException {
 		for (File file : stagingDir.listFiles()) {
 			File newCommitPath = new File(newCommit, file.getName());
@@ -261,11 +197,6 @@ public class Gitlet implements Serializable {
 		return false;
 	}
 
-	/*
-	 * @author
-	 * "http://examples.javacodegeeks.com/core-java/io/file/4-ways-to-copy-file-in-java/"
-	 * make a copy of source and move it to dest
-	 */
 	private static void copyFileUsingFileChannels(File source, File dest)
 			throws IOException {
 		FileChannel inputChannel = null;
