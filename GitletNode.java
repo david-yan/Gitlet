2c10fc7d37076cd6187c1037517b4e1b80efc199
david-yan <david.yan@berkeley.edu>
2015-07-27 00:05:03 -0700

    Update GitletNode.java

--- GitletNode.java
+++ GitletNode.java
@@ -4,13 +4,16 @@ import java.io.*;
 
 public class GitletNode implements Serializable
 {
-	private GitletNode			prevCommit; //previous commit node. null for first commit
+	private GitletNode			prevCommit;	// previous commit node. null
+												// for first commit
 	private String				commitMessage;
 	private String				timeStamp;
 	private int					commitID;
 	private File				folder;
-	private ArrayList<String>	nameOfFiles; //names of all of the files in this commit, 
-											 //including the ones not written to the folder
+	private ArrayList<String>	nameOfFiles;	// names of all of the files in
+												// this commit,
+												// including the ones not
+												// written to the folder
 
 	public GitletNode(String message, int ID, GitletNode prev)
 	{
@@ -22,7 +25,7 @@ public class GitletNode implements Serializable
 		folder = new File(".gitlet/commits/" + commitID);
 		folder.mkdir();
 	}
-	
+
 	public String getTimeStamp()
 	{
 		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
@@ -40,7 +43,7 @@ public class GitletNode implements Serializable
 			prevCommit.printLog();
 		}
 	}
-	
+
 	public void print()
 	{
 		System.out.println("===");
@@ -70,14 +73,16 @@ public class GitletNode implements Serializable
 	}
 
 	/**
-	 * Add file name to the list of files. 
-	 * @param fileName Name of file to add
+	 * Add file name to the list of files.
+	 * 
+	 * @param fileName
+	 *            Name of file to add
 	 */
 	public void addFile(String fileName)
 	{
 		nameOfFiles.add(fileName);
 	}
-	
+
 	/**
 	 * @return File of the folder of this commit
 	 */
@@ -85,13 +90,14 @@ public class GitletNode implements Serializable
 	{
 		return folder;
 	}
-	
-		// modified for rebase
+
+	// modified for rebase
 	public ArrayList<File> getModifiedFilesForRebase(GitletNode node)
 	{
 		GitletNode current = this;
 		ArrayList<File> toReturn = new ArrayList<File>();
-		while(current != node) {
+		while (current != node)
+		{
 			for (File file : current.getFolder().listFiles())
 				if (!toReturn.contains(file.getName()))
 					toReturn.add(file);
@@ -100,34 +106,41 @@ public class GitletNode implements Serializable
 		}
 		return toReturn;
 	}
-	
+
 	// modified for rebase
-	public ArrayList<String> getNonModifiedFiles() {
+	public ArrayList<String> getNonModifiedFiles()
+	{
 		GitletNode current = this;
 		ArrayList<String> toRtn = new ArrayList<String>();
-		
+
 		// this runs in O(n^2)
-		for (String fileName : current.getFiles()) {
-			for (File modifiedFile : getFolder().listFiles()) {
-				if (!fileName.equals(modifiedFile.getName())) {
+		for (String fileName : current.getFiles())
+		{
+			for (File modifiedFile : getFolder().listFiles())
+			{
+				if (!fileName.equals(modifiedFile.getName()))
+				{
 					toRtn.add(fileName);
 				}
 			}
 		}
 		return toRtn;
 	}
-	
+
 	/**
-	 * Gets the ArrayList of the names of all of the files that have been 
+	 * Gets the ArrayList of the names of all of the files that have been
 	 * modified since the node
-	 * @param node The node to be compared with
+	 * 
+	 * @param node
+	 *            The node to be compared with
 	 * @return List of names of modified files
 	 */
 	public ArrayList<String> getModifiedFiles(GitletNode node)
 	{
 		GitletNode current = this;
 		ArrayList<String> toReturn = new ArrayList<String>();
-		while(current != node) {
+		while (current != node)
+		{
 			for (File file : current.getFolder().listFiles())
 				if (!toReturn.contains(file.getName()))
 					toReturn.add(file.getName());
@@ -136,31 +149,22 @@ public class GitletNode implements Serializable
 		}
 		return toReturn;
 	}
-	
+
 	/**
 	 * Finds and returns the file with the given name
-	 * @param fileName: The name of the file to be returned
-	 * @return Most recent occurrence of the file or null if the file does not exist
+	 * 
+	 * @param fileName
+	 *            : The name of the file to be returned
+	 * @return Most recent occurrence of the file or null if the file does not
+	 *         exist
 	 */
 	public File getFile(String fileName)
 	{
-		String updatedName = ".gitlet/commits/" + commitID + "/" + fileName;
-		updatedName = updatedName.replace("/", "\\").trim();
-		File searched = searchFolders(folder.listFiles(), updatedName);
-		if(searched != null)
-			return searched;
-		else if (prevCommit != null)
-			return prevCommit.getFile(fileName);
-		return null;
-	}
-	
-	private static File searchFolders(File[] files, String fileName){
-		for(File file: files){
-			if(file.getPath().equals(fileName))
-				return file;
-			if(file.isDirectory())
-				return searchFolders(file.listFiles(), fileName);
-		}
-		return null;
+		if (!nameOfFiles.contains(fileName))
+			return null;
+		File file = new File(".gitlet/commits/" +  commitID + "/" + fileName);
+		if (file.exists())
+			return file;
+		return prevCommit.getFile(fileName);
 	}
 }
