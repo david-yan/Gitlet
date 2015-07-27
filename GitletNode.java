33852629bfe6d5f38dd91c58930888d8fb672d53
siddharthseth <siddharth.seth@berkeley.edu>
2015-07-27 16:52:22 -0700

    Added comments to each method

--- GitletNode.java
+++ GitletNode.java
@@ -15,6 +15,12 @@ public class GitletNode implements Serializable
 												// including the ones not
 												// written to the folder
 
+	/**
+	 * Constructs a new GitletNode, keeping track of all its files
+	 * @param message - the commit message
+	 * @param ID - the commit ID
+	 * @param prev - the previous/parent GitletNode
+	 */
 	public GitletNode(String message, int ID, GitletNode prev)
 	{
 		prevCommit = prev;
@@ -26,11 +32,19 @@ public class GitletNode implements Serializable
 		folder.mkdir();
 	}
 
+	/**
+	 * Gets the current time stamp upon call
+	 * @return - String format of the commit Date
+	 */
 	public String getTimeStamp()
 	{
 		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 	}
 
+	/**
+	 * Prints out the log of the GitletNode, printing out the commit ID, message
+	 * and recursively calls its parent node. Used in log, not global log!
+	 */
 	public void printLog()
 	{
 		System.out.println("===");
@@ -44,6 +58,10 @@ public class GitletNode implements Serializable
 		}
 	}
 
+	/**
+	 * Prints out the log of the GitletNode, but does not recursively call its
+	 * parents. Used in global log, not log!
+	 */
 	public void print()
 	{
 		System.out.println("===");
@@ -52,20 +70,36 @@ public class GitletNode implements Serializable
 		System.out.println(commitMessage);
 	}
 
+	/**
+	 * Gets the previous commit node
+	 * @return - GitletNode of previous commit
+	 */
 	public GitletNode getPrevCommit()
 	{
 		return prevCommit;
 	}
+	
+	/**
+	 * Gets the commit ID of the respective commit
+	 * @return - int of the commit ID
+	 */
 	public int getID()
 	{
 		return commitID;
 	}
+	
+	/**
+	 * Gets the commit message of the respective commit
+	 * @return - String of commit message
+	 */
 	public String getMessage()
 	{
 		return commitMessage;
 	}
+	
 	/**
-	 * @return list of all files associated with this commit
+	 * Gets all of the files associated with the commit
+	 * @return ArrayList of all files associated with the commmit
 	 */
 	public ArrayList<String> getFiles()
 	{
@@ -74,9 +108,7 @@ public class GitletNode implements Serializable
 
 	/**
 	 * Add file name to the list of files.
-	 * 
-	 * @param fileName
-	 *            Name of file to add
+	 * @param fileName - the String of the name of the file to add
 	 */
 	public void addFile(String fileName)
 	{
@@ -84,14 +116,19 @@ public class GitletNode implements Serializable
 	}
 
 	/**
-	 * @return File of the folder of this commit
+	 * Returns the folder of the commit (for example .gitlet/commits/2)
+	 * @return - File of the folder of this commit
 	 */
 	public File getFolder()
 	{
 		return folder;
 	}
 
-	// modified for rebase
+	/**
+	 * Gets an ArrayList of Files that have been modified
+	 * @param GitletNode - the other commit node 
+	 * @return ArrayList - list of files that have been modified
+	 */
 	public ArrayList<File> getModifiedFilesForRebase(GitletNode node)
 	{
 		GitletNode current = this;
@@ -101,39 +138,16 @@ public class GitletNode implements Serializable
 			for (File file : current.getFolder().listFiles())
 				if (!toReturn.contains(file.getName()))
 					toReturn.add(file);
-			// fixed
 			current = current.prevCommit;
 		}
 		return toReturn;
 	}
 
-	// modified for rebase
-	public ArrayList<String> getNonModifiedFiles()
-	{
-		GitletNode current = this;
-		ArrayList<String> toRtn = new ArrayList<String>();
-
-		// this runs in O(n^2)
-		for (String fileName : current.getFiles())
-		{
-			for (File modifiedFile : getFolder().listFiles())
-			{
-				if (!fileName.equals(modifiedFile.getName()))
-				{
-					toRtn.add(fileName);
-				}
-			}
-		}
-		return toRtn;
-	}
-
 	/**
-	 * Gets the ArrayList of the names of all of the files that have been
+	 * Gets the LinkedList of the names of all of the files that have been
 	 * modified since the node
-	 * 
-	 * @param node
-	 *            The node to be compared with
-	 * @return List of names of modified files
+	 * @param GitletNode - the node to be compared with
+	 * @return LinkedList - list of names of modified files
 	 */
 	public LinkedList<String> getModifiedFiles(GitletNode node)
 	{
@@ -147,7 +161,6 @@ public class GitletNode implements Serializable
 				if (file.exists() && !toReturn.contains(fileName))
 					toReturn.add(fileName);
 			}
-			// fixed
 			current = current.prevCommit;
 		}
 		return toReturn;
@@ -155,9 +168,7 @@ public class GitletNode implements Serializable
 
 	/**
 	 * Finds and returns the file with the given name
-	 * 
-	 * @param fileName
-	 *            : The name of the file to be returned
+	 * @param fileName - the name of the file to be returned
 	 * @return Most recent occurrence of the file or null if the file does not
 	 *         exist
 	 */
